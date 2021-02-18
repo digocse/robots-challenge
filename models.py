c0a8dbcb5cf6d5b4ec6d9d72b4a12da4d314d558
Rodrigo Santos <santos_ro@hotmail.com>
2021-02-17 21:20:17 -0300

    feat: create Robot class and robots property to store Robots at the grid

--- models.py
+++ models.py
@@ -2,3 +2,17 @@ class Grid:
 	def __init__(self, x: int, y:int):
 		self.max_x = x
 		self.max_y = y
+		self.robots = []
+
+
+class Robot:
+	COMPASS = ['N', 'E', 'S', 'W']
+	DIRECTION = [1, 1, -1, -1]
+
+	def __init__(self, x=0, y=0, orientation='N'):
+		self.x = x
+		self.y = y
+		self.orientation = Robot.COMPASS.index(orientation)
+
+	def pos(self):
+		return (self.x, self.y, Robot.COMPASS[self.orientation])
