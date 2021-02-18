f785e7a8b20cf3c1205ed844dcae6bab6029fcb9
Rodrigo Santos <santos_ro@hotmail.com>
2021-02-17 21:23:02 -0300

    feat: create helper methods to  generate robot, check if coordination is outside the grid and get Robots last registered positions

--- models.py
+++ models.py
@@ -4,6 +4,23 @@ class Grid:
 		self.max_y = y
 		self.robots = []
 
+	def generate_robot(self, x: int, y: int, orientation: str):
+
+		if (x,y) in self.last_positions():
+			print('The robot has crashed with another one')
+
+		# if self.
+
+	def is_outside_grid(self, x: int, y:int):
+		return x < 0 or y < 0 or x > self.max_x or y > self.max_y
+
+
+	def last_positions(self):
+		return [
+			robot.pos()[:-1]
+			for robot in self.robots
+		]
+
 
 class Robot:
 	COMPASS = ['N', 'E', 'S', 'W']
